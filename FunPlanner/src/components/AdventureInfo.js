488900d4e898acc416ef86785d2642acc58e52c1
Kevin <kjs2b@virginia.edu>
2017-05-15 18:54:04 -0700

    render category-specific icon on AdventureInfo or render generic icon if category icon cannot be found

--- FunPlanner/src/components/AdventureInfo.js
+++ FunPlanner/src/components/AdventureInfo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//const icon = require('../../public/icons/Hikes.png');
 
 export default class AdventureInfo extends Component {
   constructor(props) {
@@ -6,11 +7,26 @@ export default class AdventureInfo extends Component {
     this.state = {};
   }
 
+  findIcon() {
+    try {
+      return require('../../public/icons/' + this.props.adventure.category + '.png');
+    }
+    catch (err) {
+      return require('../../public/icons/Unknown.png');
+    }
+  }
+
 
   render() {
     return (
       <div>
-        Adventure: {this.props.adventure.title}
+        <h2>{this.props.adventure.title}</h2>
+        <h4>{this.props.adventure.location}</h4>
+        <h6>{this.props.adventure.category}</h6>
+        <img
+          src={this.findIcon()}
+          className='adventureIcon'
+        />
       </div>
     );
   }
