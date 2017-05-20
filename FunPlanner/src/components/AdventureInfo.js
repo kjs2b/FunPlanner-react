420530936326bd511fcd1d1e7c69ba97646bbedc
Kevin <kjs2b@virginia.edu>
2017-05-20 12:40:22 -0700

    (nonfunctional) Edit and Delete buttons added to AdventureInfo

--- FunPlanner/src/components/AdventureInfo.js
+++ FunPlanner/src/components/AdventureInfo.js
@@ -5,6 +5,9 @@ export default class AdventureInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   findIcon() {
@@ -14,6 +17,14 @@ export default class AdventureInfo extends Component {
     catch (err) {
       return require('../../public/icons/Unknown.png');
     }
+  }
+
+  handleEdit() {
+    console.log("edit");
+  }
+
+  handleDelete() {
+    console.log('Delete');
   }
 
 
@@ -33,6 +44,22 @@ export default class AdventureInfo extends Component {
           {this.props.adventure.link}
         </a>
         <p>Notes: {this.props.adventure.notes}</p>
+        <div className='editButtonsConatainer'>
+          <button
+            type='button'
+            className='editDeleteButton'
+            onClick={this.handleEdit}
+          >
+            Edit
+          </button>
+          <button
+            type='button'
+            className='editDeleteButton'
+            onClick={this.handleDelete}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
