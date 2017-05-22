d713b4d22241d1a1ba3099de9ee20dc590ed422c
Kevin <kjs2b@virginia.edu>
2017-05-21 17:15:04 -0700

    Stripped-down Login page added

--- FunPlanner/src/App.js
+++ FunPlanner/src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, IndexRedirect } from 'react-router-dom';
 
+import AuthService from './utils/AuthService'
 import Adventures from './components/Adventures';
 import Nav from './components/Nav';
 import AddAdventureContainer from './components/AddAdventureContainer';
+import Login from './components/Login';
+
+
+const auth = new AuthService('v9P8Ehko9qtNgpFuEY-uYrmrr8puOyOy', 'kjs2b.auth0.com');
+
+const requireAuth = (nextState, replace) => {
+  if (!auth.loggedIn()) {
+    replace({ pathname: '/login' });
+  }
+}
 
 class App extends Component {
   render() {
--- Funplanner/src/components/Login.js
+++ Funplanner/src/components/Login.js
@@ -1,22 +1,13 @@
 import React, { PropTypes as T } from 'react'
 import {ButtonToolbar, Button} from 'react-bootstrap'
-import AuthService from 'utils/AuthService'
-import styles from './styles.module.css'
+import AuthService from '../utils/AuthService'
 
 export class Login extends React.Component {
-  static propTypes = {
-    location: T.object,
-    auth: T.instanceOf(AuthService)
-  }
-
   render() {
     const { auth } = this.props
     return (
-      <div className={styles.root}>
+      <div>
         <h2>Login</h2>
-        <ButtonToolbar className={styles.toolbar}>
-          <Button bsStyle="primary" onClick={auth.login.bind(this)}>Login</Button>
-        </ButtonToolbar>
       </div>
     )
   }
