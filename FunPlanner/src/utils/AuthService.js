13ef527b980af132fe8df6216ff70ddd56427828
Kevin <kjs2b@virginia.edu>
2017-05-23 12:47:28 -0700

    updated AuthService with environmentally stored variables for security

--- FunPlanner/src/utils/AuthService.js
+++ FunPlanner/src/utils/AuthService.js
@@ -1,44 +1,93 @@
-import Auth0Lock from 'auth0-lock'
-import { browserHistory } from 'react-router'
+import decode from 'jwt-decode';
+import { browserHistory } from 'react-router';
+import auth0 from 'auth0-js';
+const secrets = require('../../../server/secrets');
+const ID_TOKEN_KEY = 'id_token';
+const ACCESS_TOKEN_KEY = 'access_token';
 
-export default class AuthService {
-  constructor(clientID, domain) {
-    //Configure Auth
-    this.lock = new Auth0Lock(clientID, domain, {
-      auth: {
-        redirectUrl: 'http://localhost:3000/login',
-        responseType: 'token'
-      }
-    });
-    //Add callback for authenticated event
-    this.lock.on('authenticated', this._doAuthentication.bind(this));
-    this.login = this.login.bind(this);
-  }
+const CLIENT_ID = secrets.clientID;
+const CLIENT_DOMAIN = secrets.issuer;
+//may need to change this to 3001 since I'm using a proxy for api requests
+const REDIRECT = 'http://localhost:3000/callback';
+const SCOPE = secrets.scope;
+const AUDIENCE = secrets.audience;
 
-  _doAuthentication(authResult) {
-    //Save user token
-    this.setToken(authResult.idToken);
-    //navigate to home route
-    browserHistory.replace('/home');
-  }
+var auth = new auth0.WebAuth({
+  clientID: CLIENT_ID,
+  domain: CLIENT_DOMAIN
+});
 
-  login() {
-    this.lock.show();
-  }
+export function login() {
+  auth.authorize({
+    responseType: 'token id_token',
+    redirectUri: REDIRECT,
+    audience: AUDIENCE,
+    scope: SCOPE
+  });
+}
 
-  loggedIn() {
-    return !!this.getToken();
-  }
+export function logout() {
+  clearIdToken();
+  clearAccessToken();
+  browserHistory.push('/');
+}
 
-  setToken(idToken) {
-    localStorage.setItem('id_token', idToken);
+export function requireAuth(nextState, replace) {
+  if (!isLoggedIn()) {
+    replace({pathname: '/'});
   }
+}
 
-  getToken() {
-    return localStorage.getItem('id_token');
-  }
+export function getIdToken() {
+  return localStorage.getItem(ID_TOKEN_KEY);
+}
 
-  logout() {
-    localStorage.removeItem('id_token');
-  }
+export function getAccessToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
+
+function clearIdToken() {
+  localStorage.removeItem(ID_TOKEN_KEY);
+}
+
+function clearAccessToken() {
+  localStorage.removeItem(ACCESS_TOKEN_KEY);
+}
+
+// Helper function that will allow us to extract the access_token and id_token
+function getParameterByName(name) {
+  let match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+// Get and store access_token in local storage
+export function setAccessToken() {
+  let accessToken = getParameterByName('access_token');
+  localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+}
+
+// Get and store id_token in local storage
+export function setIdToken() {
+  let idToken = getParameterByName('id_token');
+  localStorage.setItem(ID_TOKEN_KEY, idToken);
+}
+
+export function isLoggedIn() {
+  const idToken = getIdToken();
+  return !!idToken && !isTokenExpired(idToken);
+}
+
+function getTokenExpirationDate(encodedToken) {
+  const token = decode(encodedToken);
+  if (!token.exp) { return null; }
+
+  const date = new Date(0);
+  date.setUTCSeconds(token.exp);
+
+  return date;
+}
+
+function isTokenExpired(token) {
+  const expirationDate = getTokenExpirationDate(token);
+  return expirationDate < new Date();
 }
